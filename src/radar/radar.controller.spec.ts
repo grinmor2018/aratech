f111270283975de4d8a562e962b13aba8e2def7c
David Marquez <grinmor2016@gmail.com>
2021-12-12 20:35:48 +0100

    test

--- src/radar/radar.controller.spec.ts
+++ src/radar/radar.controller.spec.ts
@@ -1,11 +1,15 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Coordinates } from './../models/coordinates';
+import { Envio } from './../models/envio';
 import { RadarController } from './radar.controller';
+import { RadarService } from './radar.service';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('RadarController', () => {
   let controller: RadarController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [{ provide: RadarService, useValue: {} }],
       controllers: [RadarController],
     }).compile();
 
