6da546c431935d9eaa040969abcc90be0a0de942
David Marquez <grinmor2016@gmail.com>
2021-12-12 20:36:13 +0100

    test

--- src/radar/radar.controller.ts
+++ src/radar/radar.controller.ts
@@ -1,8 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { Coordinates } from 'src/models/coordinates';
 import { Envio } from './../models/envio';
 import { RadarService } from './radar.service';
-/* eslint-disable @typescript-eslint/no-empty-function */
 
 @Controller('radar')
 export class RadarController {
